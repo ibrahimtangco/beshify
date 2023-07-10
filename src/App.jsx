db250044300ac179329f8d8b4d0645087d7f7d02
Ibrahim Tangco <barangobong13@gmail.com>
2023-07-10 17:12:45 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import BeshifyComponent from "./components/BeshifyComponent";
 function App() {
     return (
-        <div className="bg-[#618298] h-screen py-20 md:flex items-center">
+        <div className="bg-[#618298] min-h-screen py-20 md:flex items-center">
             <BeshifyComponent />
         </div>
     );
